175e0ce2f40c64b8642dd58e0e73a2319663917f
RLaghSoftware <82481988+RLaghSoftware@users.noreply.github.com>
2023-04-05 15:51:39 -0500

    Update database.dart

--- lib/service/database.dart
+++ lib/service/database.dart
@@ -20,36 +20,63 @@ class DatabaseService {
       FirebaseFirestore.instance.collection('Growth');
   final CollectionReference temperatureCollection =
       FirebaseFirestore.instance.collection('Temperature');
-  final CollectionReference throwUpCollection =
+  final CollectionReference ThrowupCollection =
       FirebaseFirestore.instance.collection('Throwup');
-  final CollectionReference vaccineCollection =
+  final CollectionReference VaccineCollection =
       FirebaseFirestore.instance.collection('Vaccine');
 
   Future updateDiaperMetric(DiaperMetricModel model) async {
     return await diaperCollection.doc().set(model.toJson());
   }
 
-  Future updateFooodMetric(FoodMetricModel model) async {
+  Future editDiaperMetric(DiaperMetricModel model, String id) async {
+    return await diaperCollection.doc(id).update(model.toJson());
+  }
+
+  Future updateFoodMetric(FoodMetricModel model) async {
     return await foodCollection.doc().set(model.toJson());
+  }
+
+   Future editFoodMetric(FoodMetricModel model, String id) async {
+    return await foodCollection.doc(id).update(model.toJson());
   }
 
   Future updateSleepMetric(SleepMetricModel model) async {
     return await sleepCollection.doc().set(model.toJson());
   }
 
+  Future editSleepMetric(SleepMetricModel model, String id) async {
+    return await sleepCollection.doc(id).update(model.toJson());
+  }
+
+
   Future updateGrowthMetric(GrowthMetricModel model) async {
     return await growthCollection.doc().set(model.toJson());
+  }
+
+  Future editGrowthMetric(GrowthMetricModel model, String id) async {
+    return await growthCollection.doc(id).update(model.toJson());
   }
 
   Future updateTemperatureMetric(TempMetricModel model) async {
     return await temperatureCollection.doc().set(model.toJson());
   }
 
-  Future updateThrowUpMetric(ThrowUpMetricModel model) async {
-    return await throwUpCollection.doc().set(model.toJson());
+  Future editTemperatureMetric(TempMetricModel model, String id) async {
+    return await temperatureCollection.doc(id).update(model.toJson());
   }
 
+  Future updateThrowUpMetric(ThrowUpMetricModel model) async {
+    return await ThrowupCollection.doc().set(model.toJson());
+  }
+
+  Future editThrowUpMetric(ThrowUpMetricModel model, String id) async {
+    return await ThrowupCollection.doc(id).update(model.toJson());
+  }
   Future updateVaccineMetric(VaccineMetricModel model) async {
-    return await vaccineCollection.doc().set(model.toJson());
+    return await VaccineCollection.doc().set(model.toJson());
+  }
+  Future editVaccineMetric(VaccineMetricModel model, String id) async {
+    return await VaccineCollection.doc(id).update(model.toJson());
   }
 }
